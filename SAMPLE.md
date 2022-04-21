d887fd2418ab1b0c025977b9c0c06f338b5f5b0e
Eli Wood <mreliwood1@gmail.com>
2022-04-20 20:56:54 -0700

    completed README generator and supporting documents

---
+++ SAMPLE.md
@@ -0,0 +1,39 @@
+# The Weather Dashboard &nbsp; ![MIT License](https://img.shields.io/badge/license-MIT-green)
+    
+## Description
+
+The Weather Dashboard is a simple website that displays today's weather and a 5-day forecast. By default the location is set to New York, however the user can search for any city in the US and some from accross the globe.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+This project is deployed using github pages and doesn't require any installations. It does use some basic jQuery and Moment.js so don't forget those links!
+
+## Usage
+
+When the user lands on The Weather Dashboard, they will immediately see the weather for New York today and for the next five days. The user can enter the name of a city in the search bar located on the left side of the screen. The user's most recent searches are saved in local storage for easy reference and will be displayed just below the search bar. The user will also be presented with the option to clear their search history after making their first search.
+
+## License
+
+MIT License
+Copyright &copy; 2022 The Weather Dashboard
+
+## Contributing
+
+jQuery, Moment.js
+
+## Tests
+
+Searching for things that are not cities, saving very large amounts of searches before clearing the history, searching for cities that are not within the US, searching for the same city multiple times.
+
+## Questions
+
+[MrEliWood on GitHub](https://github.com/MrEliWood) | [contact.eliwood@gmail.com](mailto:contact.eliwood@gmail.com)
---
+++ index.js
@@ -0,0 +1,123 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// create file
+inquirer
+  .prompt([
+
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please write a detailed description of your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the necessary steps to install your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please explain how to use your project in detail.',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Choose a license.',
+        choices: ['MIT License', 'GNU GPLv3'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'List any/all contributors and resources that helped in development.',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'What tests did you perform to measure the performance of your project?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    
+  ])
+  .then(function(response) {
+
+    let {title, description, installation, usage, license, contributing, tests, github, email} = response;
+
+    let chosenLicense;
+    let licenseAlt;
+    let licenseImg;
+
+    if (license === 'MIT License') {
+
+        chosenLicense = `MIT License\nCopyright &copy; 2022 ${title}`;
+        licenseAlt = 'MIT License';
+        licenseImg = 'https://img.shields.io/badge/license-MIT-green'
+
+    } else {
+
+        chosenLicense = `GNU GENERAL PUBLIC LICENSE\nVersion 3, 29 June 2007`
+        licenseAlt = 'GNU GPLv3';
+        licenseImg = 'https://img.shields.io/badge/license-GPL-blue'
+
+    }
+
+    let readmeMarkdown =
+    
+`# ${title} &nbsp; ![${licenseAlt}](${licenseImg})
+    
+## Description
+
+${description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## License
+
+${chosenLicense}
+
+## Contributing
+
+${contributing}
+
+## Tests
+
+${tests}
+
+## Questions
+
+[${github} on GitHub](https://github.com/${github}) &nbsp; | &nbsp; [${email}](mailto:${email})`
+
+    fs.writeFile('SAMPLE.md', readmeMarkdown, function(error) {
+        if (error) {console.error(error)};
+    });
+
+    });
